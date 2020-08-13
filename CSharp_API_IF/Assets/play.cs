caf2d134b64efd9715dc6d82897b39a8fb787406
chiang8610 <67904649+chiang8610@users.noreply.github.com>
2020-08-13 16:42:27 +0800

    0813作業練習 3D走路

---
+++ CSharp_API_IF/Assets/play.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 1000)]
+    public float speed = 5;
+
+    public Animator ani;
+
+
+    public void Move()
+    {
+
+        bool w = Input.GetKey(KeyCode.W);
+        ani.SetBool("走路開關", w);
+
+  
+
+
+
+
+    }
+
+    private void Update()
+    {
+
+        Move();
+    }
+    }
